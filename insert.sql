3fe93fb5887f4f55aab2c04231c2b02880d20b6f
Abdurrahan Saleh <abi_abdurrahman@hotmail.com>
2017-04-26 13:56:17 +0700

    commit insert jenjang and program_studi

---
+++ insert.sql
@@ -0,0 +1,26 @@
+INSERT INTO JENJANG(nama) VALUES 
+	('S1'),
+	('S2'),
+	('S3');
+
+INSERT INTO PROGRAM_STUDI(kode, nama, jenis_kelas, nama_fakultas, jenjang) VALUES
+	(1, 'Kedokteran', 'Reguler', 'Kedokteran', 'S1'),
+	(2, 'Matematika', 'Reguler', 'Matematika dan Pengetahuan Alam', 'S1'),
+	(3, 'Teknik Sipil', 'Reguler', 'Teknik', 'S1'),
+	(4, 'Ilmu Komputer', 'Reguler', 'Ilmu Komputer', 'S1'),
+	(5, 'Kedokteran', 'Reguler', 'Kedokteran', 'S2'),
+	(6, 'Fisika', 'Reguler', 'Matematika dan Pengetahuan Alam', 'S2'),
+	(7, 'Teknik Sipil', 'Reguler', 'Teknik', 'S2'),
+	(8, 'Ilmu Komputer', 'Reguler', 'Ilmu Komputer', 'S2'),
+	(9, 'Kedokteran', 'Reguler', 'Kedokteran', 'S3'),
+	(10, 'Biologi', 'Reguler', 'Matematika dan Pengetahuan Alam', 'S3'),
+	(11, 'Teknik Sipil', 'Reguler', 'Teknik', 'S3'),
+	(12, 'Ilmu Komputer', 'Reguler', 'Ilmu Komputer', 'S3'),
+	(13, 'Kedokteran', 'Internasional', 'Kedokteran', 'S1'),
+	(14, 'Biologi', 'Internasional', 'Matematika dan Pengetahuan Alam', 'S1'),
+	(15, 'Teknik Industri', 'Internasional', 'Teknik', 'S1'),
+	(16, 'Ilmu Komputer', 'Internasional', 'Ilmu Komputer', 'S1'),
+	(17, 'Kedokteran', 'Paralel', 'Kedokteran', 'S1'),
+	(18, 'Biologi', 'Paralel', 'Matematika dan Pengetahuan Alam', 'S1'),
+	(19, 'Teknik Industri', 'Paralel', 'Teknik', 'S1'),
+	(20, 'Ilmu Komputer', 'Paralel', 'Ilmu Komputer', 'S1');
