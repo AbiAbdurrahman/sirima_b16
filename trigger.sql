7fb65cb0ecefe7b040b7944a2fdd502149ba0e90
Abdurrahan Saleh <abi_abdurrahman@hotmail.com>
2017-04-29 22:22:58 +0700

    commit revisi create dan trigger

--- trigger.sql
+++ trigger.sql
@@ -1,11 +1,20 @@
 CREATE OR REPLACE FUNCTION UpdateJumlahPelamar()
-RETURNS "trigger" AS
+RETURNS trigger AS
 $$
+	DECLARE
+		jml_pelamar integer;
 	BEGIN
-	
+
 	IF(TG_OP = 'INSERT')THEN
+		SELECT jumlah_pelamar into jml_pelamar 
+		FROM PENERIMAAN_PRODI PR, PENDAFTARAN P
+		WHERE new.kode_prodi = PR.kode_prodi and
+		 new.id_pendaftaran = P.id and P.nomor_periode = PR.nomor_periode;
+		IF(jml_pelamar = NULL)THEN
+			jml_pelamar = 0;
+		END IF;
 		UPDATE PENERIMAAN_PRODI PR
-		SET jumlah_pelamar = jumlah_pelamar +1
+		SET jumlah_pelamar = jml_pelamar +1
 		WHERE new.kode_prodi = PR.kode_prodi and
 		 new.id_pendaftaran = PENDAFTARAN.id and
 		 PR.tahun_periode = PENDAFTARAN.tahun_periode;
--- trigger2.sql
+++ trigger2.sql
@@ -1,10 +1,19 @@
 CREATE OR REPLACE FUNCTION UpdateJumlahDiterima()
 RETURNS trigger AS
 $$
+	DECLARE
+		jml_diterima integer;
 	BEGIN
 	IF(new.status_lulus <> old.status_lulus && new.status_lulus = TRUE)THEN
+		SELECT jumlah_pelamar into jml_diterima 
+		FROM PENERIMAAN_PRODI PR, PENDAFTARAN P
+		WHERE new.kode_prodi = PR.kode_prodi and
+		 new.id_pendaftaran = P.id and P.nomor_periode = PR.nomor_periode;
+		IF(jml_diterima = NULL)THEN
+			jml_diterima = 0;
+		END IF;
 		UPDATE PENERIMAAN_PRODI PR
-		SET jumlah_diterima = jumlah_diterima +1
+		SET jumlah_diterima = jml_diterima +1
 		WHERE new.kode_prodi = PR.kode_prodi and
 		 new.id_pendaftaran = PENDAFTARAN.id and
 		 PR.tahun_periode = PENDAFTARAN.tahun_periode;
@@ -31,8 +40,7 @@ END;
 $$
 LANGUAGE 'plpgsql' VOLATILE;
 
-
-CREATE TRIGGER UpdateJumlahDiterima() 
+CREATE TRIGGER UpdateJumlahDiterima 
 AFTER UPDATE
 ON  PENDAFTARAN_PRODI
 EXECUTE PROCEDURE UpdateJumlahPelamar();
